261d8dc41c3635b79385e868614bc6cd9bc21e33
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2026-06-01 17:24:05 +0530

    Update index.routes.ts

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import authRoutes from "./auth.routes"
-import checkerRoutes from "./checker.routes"
-import { AuthController } from "../controllers/auth.controllers"
+import authRoutes from "./auth.routes.js"
+import checkerRoutes from "./checker.routes.js"
+import stakeRoutes from "./staking.routes.js";
+import webhookRoutes from "./webhook.routes.js";
+import { AuthController } from "../controllers/auth.controllers.js"
 
 const router = Router()
 
 router.use("/api/auth", authRoutes);
 router.use("/api/checker", checkerRoutes);
+router.use("/api/staking", stakeRoutes);
+router.use("/api/webhooks", webhookRoutes);
 router.get("/", AuthController.root);
-
 export default router
